a7a49fee4bdcc09757c21db878d23251153fa497
Marcel <googlillo@gmail.com>
2021-02-23 10:24:05 +0100

    Add post form on top of the table

--- views/patients/patientsDashboard.php
+++ views/patients/patientsDashboard.php
@@ -1,7 +1,4 @@
 <div id="patients-dashboard" class="patients-dashboard">
-<div class="d-grid gap-2">
-  <a class="btn btn-success" type="button" href="">NEW PATIENT</a>
-</div>
     <table class="table">
         <thead class="table-dark">
             <tr>
@@ -15,6 +12,34 @@
             </tr>
         </thead>
         <tbody>
+        <form action="../../test.php" id="add-patient" method="POST">
+        <tr>
+            <td>
+                <input type="text" class="form-control" id="add-patient__name" form="add-patient" placeholder="" required>
+            </td>
+            <td>
+                <input type="text" class="form-control" id="add-patient__last-name" form="add-patient" placeholder="" required>
+            </td>                                        
+            <td>                                         
+                <input type="email" class="form-control"id="add-patient__email"  form="add-patient" placeholder="name@example.com" required>
+            </td>                                        
+            <td>                                         
+                <select input type="text" class="form-control" id="add-patient__gender" form="add-patient"  placeholder="" required>
+                    <option value="male">male</option>
+                    <option value="female">female</option>
+                </select>
+            </td>
+            <td>
+                <input type="number" class="form-control" id="add-patient__age" form="add-patient"  placeholder="" required>
+            </td>
+            <td>
+                <input type="text" class="form-control" id="add-patient__city" form="add-patient"  placeholder="" required>
+            </td>
+            <td>
+              <input class="btn btn-success" form="add-patient" type="submit"></input>
+            </td >
+        </tr>
+        </form>
 <?php
 foreach ($patients as $patient) {
     echo '<tr>';
